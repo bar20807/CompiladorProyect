bc74bffb7e4ae7895f900011a73d45229fedeee5
José Rodrigo Barrera <bar20807@uvg.edu.gt>
2023-04-27 18:58:03 -0600

    Reconoce valores que no son secuencias ASCII.

--- YalexReader.py
+++ YalexReader.py
@@ -102,7 +102,67 @@ class YALexGenerator(object):
                             # Se agrega el último valor del rango sin separador de OR
                             self.let_values.append(char_to_ascii2) 
                         # Se avanza al siguiente caracter del charset para buscar el siguiente rango de caracteres    
-                        iterador += 1  
+                        iterador += 1
+                #Si no se encontró ningún guión, entonces se procederá a analizar los valores que se encuentran dentro de los corchetes
+                else:
+                    #print("Detecte que es un charset con comillas simples, pero no hay guiones")
+                    print("Valor del charset: ", valor)
+                    #En este caso se analizarán todos los valores que no contengan guion, es decir, son todos aquellos valores como [' ''\t''\n'] los cuales deberán 
+                    #Ser analizados para poder obtener su ASCII y así manejar mejor el tipo de char
+                    # Se crea un ciclo while para iterar con respecto a la cantidad de ciclos de caracteres
+                    iterador = 1
+                    #Variables para guardar los caracteres iniciales y finales de un rango de caracteres en el charset
+                    primer_char = ""
+                    segundo_char = ""
+                    comilla_simple1 = 0
+                    comilla_simple2 = 0
+                    """
+                    iterador, primer_char, segundo_char, comilla_simple1, y comilla_simple2 son variables que se inicializan para su posterior uso en el ciclo while. 
+                    iterador se inicializa en 1, porque el primer caracter a analizar es el segundo caracter del charset (el primer caracter es el corchete de apertura).
+                    """
+                    while iterador < len(valor) and valor[iterador] != ']':
+                        # Se verifica si el carácter actual es una comilla simple y si no se ha encontrado ya una comilla simple previamente
+                        if valor[iterador] == "'" and not comilla_simple1:
+                            comilla_simple1 = iterador
+                        # Si ya se encontró una comilla simple previamente, se establece el valor del índice de la segunda comilla simple
+                        elif valor[iterador] == "'" and comilla_simple1:
+                            comilla_simple2 = iterador
+                            caracteres = []
+                            # Se itera sobre los caracteres entre las comillas simples y se agregan a una lista 'caracteres'
+                            for i in range(comilla_simple1 + 1, comilla_simple2):
+                                caracteres.append(valor[i])
+                            # Se une los caracteres en un string 'primer_char'
+                            primer_char = ''.join(caracteres)
+                            # Se restablecen los valores de 'comilla_simple1' y 'comilla_simple2'
+                            comilla_simple1 = 0
+                            comilla_simple2 = 0
+                            # Si 'primer_char' contiene un espacio en blanco, se reemplaza por el valor entero del espacio en blanco
+                            if primer_char.isspace():
+                                primer_char = ord(primer_char)
+                            
+                            # Si 'primer_char' comienza con una barra invertida, se elimina la barra y se reemplaza por el valor correspondiente
+                            # (newline = \n, tab = \t)
+                            elif (primer_char.startswith("\\")):
+                                primer_char = primer_char.replace("\\", '')
+                                if(primer_char == "n"):
+                                    primer_char = ord("\n")
+                                elif(primer_char == "t"):
+                                    primer_char = ord("\t")
+                            # Se imprime el mensaje "Nuevos valores encontrados: " seguido de la lista de valores actuales en 'let_values'
+                            print("Nuevos valores encontrados: ", self.let_values)
+                            # Se agrega el valor de 'primer_char' a la lista 'let_values'
+                            self.let_values.append(primer_char)
+                            
+                            # Si el siguiente carácter no es ']', se agrega un '|' a 'let_values' para separar los valores
+                            if iterador + 1 != len(valor) and valor[iterador + 1] != ']':
+                                self.let_values.append("|")
+                        # Se incrementa el valor de 'iterador' en 1
+                        iterador += 1
+
+
+                    
+                    
+                    
 
                 
 yalex_test = YALexGenerator("./Archivos Yal/slr-1.yal")
